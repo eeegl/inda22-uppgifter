f3350af1c25af295d4722df530734d5c4944cdf4
ornse <ornse@kth.se>
2023-03-23 22:48:26 +0100

    Skapade mapp för palinda-1

---
+++ palinda-1/boll.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "time"
+)
+
+// TODO
+// Implementera metoden så att den skickar "bollar"
+// till kanalen.
+// Funktionen ska skriva ut när den skickar en boll.
+// Funktionen ska även skriva ut numret på bollen.
+// Tänk på vilken typ bollen ska ha.
+func kastare(ch chan<- string) {
+    // ...
+}
+ 
+// TODO
+// Implementera metoden så att den tar emot "bollar"
+// från kanalen.
+// Funktionen ska skriva ut när den tar emot en boll.
+// Funktionen ska även skriva ut numret på bollen.
+func fångare(ch <-chan string) {
+    // ...
+}
+
+func main() {
+    c := make(chan string)
+    go kastare(c)
+    go fångare(c)
+    select {} // För att go-rutinerna ska hinna köras
+}
+
