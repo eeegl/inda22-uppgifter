9e3f124a3381848465cfc03eea92ce23639acfb3
ornse <ornse@kth.se>
2023-03-30 15:28:54 +0200

    Uppgift palinda-2

---
+++ palinda-2/binary.go
@@ -0,0 +1,17 @@
+package main
+
+// TODO: importera nödvändiga bibliotek
+
+/*
+ * Genererar och printar en slumpmässig binär sekvens (ettor och nollor).
+ * Använder kanaler.
+ */
+func printRandomBinary() {
+	// TODO: generera en slumpad sekvens av ettor och nollor
+	// TODO: printa sekvensen
+	// TOOD (extra): printa varje tal ett i taget, med en kort paus mellan varje
+}
+
+func main()  {
+	printRandomBinary()
+}
